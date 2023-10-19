689decf784df7287185c28f873e04e14a25d54e7
Till Schlueter <till.schlueter@cispa.de>
2023-10-19 14:19:37 +0200

    Make ARMv8 CCR easier to use: Enable CCR in the module directly.

--- covert-channel/kernel-modules/rpi4-module-ccr/armv8/ccr8.c
+++ covert-channel/kernel-modules/rpi4-module-ccr/armv8/ccr8.c
@@ -11,15 +11,50 @@
 #define ARMV8_PMUSERENR_ER (1<<3)
 
 void enable_ccr(void *info) {
+	uint32_t value = 0;
+
 	// Allow enabling the cycle count register from user mode:
 	// Set bit 0 (EN), 2 (CR) and 3 (ER) in PMUSERENR_EL0
 	asm volatile("MSR PMUSERENR_EL0, %0" : : "r"((u64)ARMV8_PMUSERENR_EN|ARMV8_PMUSERENR_CR|ARMV8_PMUSERENR_ER));
+
+	// Enable the CCR immediately
+
+	// Enable Performance Counter
+	asm volatile("MRS %0, PMCR_EL0" : "=r" (value));
+	value |= (1 << 0); // Enable
+	value |= (1 << 1); // Cycle counter reset
+	value |= (1 << 2); // Reset all counters
+	asm volatile("MSR PMCR_EL0, %0" : : "r" (value));
+
+	// Enable cycle counter register
+	asm volatile("MRS %0, PMCNTENSET_EL0" : "=r" (value));
+	value |= (1 << 31);
+	asm volatile("MSR PMCNTENSET_EL0, %0" : : "r" (value));
 }
 
 void disable_ccr(void *info) {
+	uint32_t value = 0;
+	uint32_t mask = 0;
+
 	// Disallow enabling the cycle count register from user mode
 	// Set PMUSERENR_EL0 to 0
 	asm volatile("MSR PMUSERENR_EL0, %0" : : "r"((u64)0));
+
+	// Disable the CCR immediately
+
+	// Disable Performance Counter
+	asm volatile("MRS %0, PMCR_EL0" : "=r" (value));
+	mask = 0;
+	mask |= (1 << 0); // Enable
+	mask |= (1 << 1); // Cycle counter reset
+	mask |= (1 << 2); // Reset all counters
+	asm volatile("MSR PMCR_EL0, %0" : : "r" (value & ~mask));
+
+	// Disable cycle counter register
+	asm volatile("MRS %0, PMCNTENSET_EL0" : "=r" (value));
+	mask = 0;
+	mask |= (1 << 31);
+	asm volatile("MSR PMCNTENSET_EL0, %0" : : "r" (value & ~mask));
 }
 
 int init_module(void) {
